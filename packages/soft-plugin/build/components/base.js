a55c68e844f4e6323e9bc7fce5dc6c9db4a42d6f
Bob Wang <wangziling1000@gmail.com>
2020-09-28 13:50:45 +0200

    info: soft-plugin @@ build

--- packages/soft-plugin/build/components/base.js
+++ packages/soft-plugin/build/components/base.js
@@ -10,7 +10,7 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 import * as React from 'react';
-import { Input } from 'antd';
+import { Input, Menu } from 'antd';
 import { strToBoolean, addToStore } from '../lib/tools';
 import { useState } from 'react';
 export default (function (type, props) {
@@ -43,6 +43,12 @@ export default (function (type, props) {
         case 'password':
             main = visible && React.createElement(Input.Password, __assign({}, props, { onChange: onChange }));
             break;
+        case 'menu-item': {
+            main = visible && React.createElement(Menu.Item, __assign({}, props),
+                " ",
+                props.content);
+            break;
+        }
         default:
             main = null;
             break;
